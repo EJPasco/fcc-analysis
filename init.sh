236086e7a5365940af5e2540f8babcba513e3344
Andrii Semkiv <semkiv@gmail.com>
2016-04-03 14:02:14 +0200

    Better init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,13 @@
-source fcc-edm/init.sh
-source heppy/init.sh
-source heppy_fcc/init.sh
-source generator/init.sh
+FCCEMD_DIR='fcc-edm'
+HEPPY_DIR='heppy'
+HEPPYFCC_DIR='heppy_fcc'
+GENERATOR_DIR='generator'
+
+echo "Sourcing ${FCCEMD_DIR}/init.sh"
+source $FCCEMD_DIR/init.sh
+echo "Sourcing ${HEPPY_DIR}/init.sh"
+source $HEPPY_DIR/init.sh
+echo "Sourcing ${HEPPYFCC_DIR}/init.sh"
+source $HEPPYFCC_DIR/init.sh
+echo "Sourcing ${GENERATOR_DIR}/init.sh"
+source $GENERATOR_DIR/init.sh
